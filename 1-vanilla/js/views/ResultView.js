3abecf7300871e53991e54ba047c78b0f0d8621c
Mengkko <aappcc1@icloud.com>
2019-06-05 23:26:51 +0900

    Search Result 2

--- 1-vanilla/js/views/ResultView.js
+++ 1-vanilla/js/views/ResultView.js
@@ -10,12 +10,22 @@ ResultView.setup = function(el) {
 
 ResultView.render = function(data = []) {
     console.log(tag, 'render()', data)
-    this.el.innerHTML = data.length ? this.getSearchResultHtml(data) : '검색 결과가 없습니다.' 
+    this.el.innerHTML = data.length ? this.getSearchResultHtml(data) : '검색 결과가 없습니다.'
+    this.show()
 }
 
 
 ResultView.getSearchResultHtml = function (data) {
-    debugger
+    return data.reduce((html, item) => {
+        html += this.getSearchItemHtml(item)
+        return  html
+    }, '<ul>') + '</ul>'
 }
 
+ResultView.getSearchItemHtml = function(item) {
+    return `<li>
+        <img src=${item.image}>
+        <p>${item.name}</p>
+    </li>`
+}
 export default ResultView
