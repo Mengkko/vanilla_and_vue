a1b72a41c35153f0d4b504f45e1d6898dbe90a9f
Mengkko <aappcc1@icloud.com>
2019-06-07 11:53:00 +0900

    주석 추가

--- 1-vanilla/js/app.js
+++ 1-vanilla/js/app.js
@@ -3,3 +3,4 @@ import MainController from './controllers/MainController.js'
 document.addEventListener('DOMContentLoaded', () => {
     MainController.init()
 })
+// 메인컨트롤러의 init()함수를 실행한다.
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # vanilla_and_vue
 
 바닐라Js와 VueJs로 같은기능을 하는 페이지를 만듭니다.
+둘의 코드를 비교하고 MVVM패턴을 공부합니다
