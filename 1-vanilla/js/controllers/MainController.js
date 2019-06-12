7b36e22eaa9675827f4777e04137149f3b93478c
mengkko <aappcc1@icloud.com>
2019-06-12 09:02:02 +0900

    HistoryView 1

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -2,9 +2,12 @@ import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
 import KeywordView from '../views/KeywordView.js'
+import HistoryView from '../views/HistoryView.js'
+
 
 import SearchModel from '../models/SearchModel.js'
 import KeywordModel from '../models/KeywordModel.js'
+import HistoryModel from '../models/HistoryModel.js'
 
 const tag = '[MainController]'
 
@@ -18,9 +21,13 @@ export default {
             .on('@change', e => this.onChangeTab(e.detail))
         KeywordView.setup(document.querySelector('#search-keyword'))
             .on('@click', e => this.onClickKeyword(e.detail.keyword))
+
+        HistoryView.setup(document.querySelector('#search-history'))
+            .on('@click', e => this.onClickHistory(e.detail.keyword))
+        
         ResultView.setup(document.querySelector('#search-result'))
         
-        this.selectedTab = '추천 검색어'
+        this.selectedTab = '최근 검색어'
         this.renderView()
     },
 
@@ -31,7 +38,7 @@ export default {
         if(this.selectedTab === '추천 검색어') {
             this.fetchSearchKeyword()
         } else{
-            debugger
+            this.fetchSearchHistory()
         }
 
         ResultView.hide()
@@ -40,6 +47,12 @@ export default {
     fetchSearchKeyword() {
         KeywordModel.list().then(data => {
             KeywordView.render(data)
+        })
+    },
+
+    fetchSearchHistory() {
+        HistoryModel.list().then(data => {
+            HistoryView.render(data)
         })
     },
 
@@ -72,6 +85,10 @@ export default {
     },
 
     onClickKeyword(keyword) {
+        this.search(keyword)
+    },
+
+    onClickHistory(keyword) {
         this.search(keyword)
     }
 }
---
+++ 1-vanilla/js/views/HistoryView.js
@@ -0,0 +1,8 @@
+import KeywordView from './KeywordView.js'
+
+const tag = '[HistoryView]'
+
+const HistoryView = Object.create(KeywordView)
+
+
+export default HistoryView
