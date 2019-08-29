a2badcf069e8d4209d3bbb6b273754debd488385
mengkko <aappcc1@icloud.com>
2019-08-29 15:57:10 +0900

    v-on click and keyup event

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -6,6 +6,14 @@ new Vue ({
     methods: {
         onSubmit(e) {
             debugger;
+        },
+        onKeyup() {
+          if(!this.query.length) this.onReset()
+        },
+        onReset() {
+            this.query = ''
+            // todo 검색결과를 숨기는 ...
+            debugger;
         }
     }
 })
